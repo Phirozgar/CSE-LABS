32375e07fea107f985bb1adee368cdf58dddabb2
Phirozgar <147513579+Phirozgar@users.noreply.github.com>
2025-01-13 00:16:37 +0530

    Update graphRepresent_2.c

--- Sem 4/DAA Lab/lab1/graphRepresent_2.c
+++ Sem 4/DAA Lab/lab1/graphRepresent_2.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-struct node{
-    int data;
+
+struct node {
+    char data;
     int index;
     int visited;
     struct node* next;
 };
 typedef struct node* Nodeptr;
 
-Nodeptr getNode(char c){
+// Function to create a new node
+Nodeptr getNode(char c) {
     Nodeptr newnode = (Nodeptr)malloc(sizeof(struct node));
     newnode->next = NULL;
     newnode->data = c;
@@ -17,73 +18,107 @@ Nodeptr getNode(char c){
     return newnode;
 }
 
-int getIndex(Nodeptr* arrayOfPointers, char c){
-    for (int i = 0; i < sizeof(arrayOfPointers); i++)
-    {
-        if(arrayOfPointers[i]->data == c) 
+// Function to get the index of a node based on its data
+int getIndex(Nodeptr* arrayOfPointers, int size, char c) {
+    for (int i = 0; i < size; i++) {
+        if (arrayOfPointers[i]->data == c) {
             return arrayOfPointers[i]->index;
+        }
     }
+    return -1; // Return -1 if not found
 }
 
-void adjacencyList(Nodeptr* arrayOfPointers){ //takes an array of Nodeptrs as argument, each Nodeptr is a vertex in the graph
-    for (int i = 0; i < sizeof(arrayOfPointers); i++)
-    {
-        Nodeptr curr = arrayOfPointers[i]; // to make the liked list at each node you need a traversing pointer to link the neighbour nodes 
-        printf("Enter number of neighbours to %c", curr->data);
+// Function to create the adjacency list
+void adjacencyList(Nodeptr* arrayOfPointers, int n) {
+    for (int i = 0; i < n; i++) {
+        Nodeptr curr = arrayOfPointers[i];
+        printf("Enter number of neighbours of %c: ", curr->data);
         int num;
         scanf("%d", &num);
-        while (num > 0)
-        {
+        getchar(); // Consume newline character left in input buffer
+        while (num > 0) {
             printf("Enter neighbour: ");
             char c;
             scanf("%c", &c);
-            Nodeptr newnode = getNode(c); // get data of neighbour and assign it some space
-            curr->next = newnode; // link the neighbour to the list
-            curr = newnode; // shift the current ptr to the newly added neighbour so next neighbour can be linked to it in the next iteration
+            getchar(); // Consume newline character
+            Nodeptr newnode = getNode(c);
+            curr->next = newnode;
+            curr = newnode;
             num--;
         }
-    } 
-    for (int i = 0; i < sizeof(arrayOfPointers); i++)
-    {
-        Nodeptr curr = arrayOfPointers[i]; // to display each node in each linked list you need a traversing ptr
-        printf("\n%c", curr->data); //display starting vertex
-        while (curr->next)
-        {
-            printf("--%c", curr->data); //display all neighbours
+    }
+
+    // Display adjacency list
+    printf("\nAdjacency List Representation:\n");
+    for (int i = 0; i < n; i++) {
+        Nodeptr curr = arrayOfPointers[i];
+        printf("%c", curr->data);
+        curr = curr->next;
+        while (curr) {
+            printf(" -> %c", curr->data);
             curr = curr->next;
         }
-    } 
-    printf("\n");
+        printf("\n");
+    }
 }
 
-void adjacencyMatrix(Nodeptr* arrayOfPointers){
-    int n = sizeof(arrayOfPointers);
+// Function to create and display the adjacency matrix
+void adjacencyMatrix(Nodeptr* arrayOfPointers, int n) {
     int grid[n][n];
 
-    for (int i = 0; i < n; i++)
-    {
-        Nodeptr curr = arrayOfPointers[i];
-        while(curr->next){
-            if(curr->next->visited == 1) continue;
-            int j = getIndex(arrayOfPointers, curr->next->data);
-            grid[i][j] = grid[j][i] = 1;
-            curr->visited = curr->next->visited = 1;
-            curr = curr->next;
-        }        
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            
+    // Initialize the adjacency matrix to 0
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            grid[i][j] = 0;
         }
-              
     }
-    
+
+    // Fill the adjacency matrix
+    for (int i = 0; i < n; i++) {
+        Nodeptr curr = arrayOfPointers[i]->next;
+        while (curr) {
+            int j = getIndex(arrayOfPointers, n, curr->data);
+            if (j != -1) {
+                grid[i][j] = 1;
+                grid[j][i] = 1; // Undirected graph
+            }
+            curr = curr->next;
+        }
+    }
+
+    // Display adjacency matrix
+    printf("\nAdjacency Matrix Representation:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", grid[i][j]);
+        }
+        printf("\n");
+    }
 }
- 
-int main(){
-    int n; 
-    
+
+int main() {
+    int n;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    getchar(); // Consume newline character
+
+    Nodeptr arrayOfPointers[n];
+
+    // Create vertices
+    for (int i = 0; i < n; i++) {
+        printf("Enter label for vertex %d: ", i + 1);
+        char c;
+        scanf("%c", &c);
+        getchar(); // Consume newline character
+        arrayOfPointers[i] = getNode(c);
+        arrayOfPointers[i]->index = i;
+    }
+
+    // Create adjacency list and display it
+    adjacencyList(arrayOfPointers, n);
+
+    // Create adjacency matrix and display it
+    adjacencyMatrix(arrayOfPointers, n);
+
     return 0;
 }
